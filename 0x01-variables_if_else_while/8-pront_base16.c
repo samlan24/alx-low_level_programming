e5b152d3dfda43ac656d81ec90c5fe73e04b25a5
allan <muchemionline@gmail.com>
2023-03-20 06:40:27 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-pront_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: Alwasy 0 when successful
+ */
+int main(void)
+{
+        int num = 0;
+        char letter = 'a';
+
+        while (num >= 0 && num <= 9)    
+        {
+                putchar(num + '0');
+                num++;
+        }
+
+        while (letter >= 'a' && letter <= 'f')
+        {
+                putchar(letter);
+                letter++;
+        }
+
+        putchar('\n');
+        return (0);
+}
