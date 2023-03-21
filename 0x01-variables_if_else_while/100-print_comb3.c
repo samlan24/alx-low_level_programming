ac07f8d6a307464f65be64136829e7b9bed03782
allan <muchemionline@gmail.com>
2023-03-20 19:51:16 -0700

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: always 0 when successful
+ */
+int main(void)
+{
+	int x, y;
+
+	for (x = 48; x < 58; x++)
+	{
+		for (y = 49; y < 58; y++)
+		{
+			if (y > x)
+			{
+				putchar(x);
+				putchar(y);
+				if (x < 56 || y < 57)
+				{
+					putchar(44);
+					putchar(32);
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
