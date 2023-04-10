2688490c50f29db98d289f40d6cad6293fdeda71
allan <muchemionline@gmail.com>
2023-04-10 06:10:19 -0700

    changes

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,12 +1,3 @@
-#ifndef MAIN_H
-#define MAIN_H
-
-/**
- *main.h file
- *
- *This is the header file with all the project's prototypes
- */
-
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
@@ -14,5 +5,6 @@ int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int is_prime_number(int n);
-
-#endif
+int _putchar(char c);
+int is_palindrome(char *s);
+int wildcmp(char *s1, char *s2);
