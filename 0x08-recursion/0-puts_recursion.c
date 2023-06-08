e9eb69e297f303645bf5ad24d85c278c848ff4fa
samlan24 <muchemionline@gmail.com>
2023-06-08 10:00:51 -0700

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -10,10 +10,10 @@ void _puts_recursion(char *s)
 	if (*s == '\0')
 	{
 		_putchar('\n');
-		return;
 	}
-
-	_putchar(*s);
-	s++;
-	_put_recursion(s);
+	else
+	{
+		_putchar(*s);
+		_puts_recursion(++s);
+	}
 }
