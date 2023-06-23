24157f876ca43135cebe092a5f311d7ed4659c5a
samlan24 <muchemionline@gmail.com>
2023-06-23 06:15:23 -0700

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -19,6 +19,8 @@ void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 typedef dog dog_t;
 dog_t *new_dog(char *name, float age, char *owner);
+char *_strcpy(char *dest, char *src);
+int _strlen(char *s);
 void free_dog(dog_t *d);
 
 #endif
