2efd803a25f80672efa3a1f2ed61ace8ae5865bf
samlan24 <muchemionline@gmail.com>
2023-06-30 05:30:08 -0700

    1-list_len.c

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -8,13 +8,13 @@
 
 size_t list_len(const list_t *h)
 {
-	size_t number_of_nodes;
+	size_t num;
 
-	number_of_nodes = 0;
-	while (h != NULL)
+	num = 0;
+	while (h)
 	{
 		h = h->next;
-		number_of_nodes++;
+		num++;
 	}
-	return (number_of_nodes);
+	return (num);
 }
