758dcdd9db05ed68afbbb66dd7cead5df4ea02fa
samlan24 <muchemionline@gmail.com>
2023-08-02 09:25:37 -0700

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,1 +1,18 @@
-heloo
+#include "main.h"
+/**
+ *_puts_recursion - prints a string
+ *@s: input
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		_putchar(*s);
+		_puts_recursion(++s);
+	}
+}
