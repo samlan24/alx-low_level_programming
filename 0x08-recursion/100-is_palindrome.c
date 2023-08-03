5bff64d362111d7e242a5c0c0a1ee2693cea805f
samlan24 <muchemionline@gmail.com>
2023-08-03 06:48:21 -0700

    100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,52 +1,51 @@
 #include "main.h"
 
 /**
- * is_palindrome - checks for a palindrome
+ * is_palindrome - check if a string is a palindrome
  * @s: char array string
- * Return: returns 1 if successful
+ * Return: 1 if palindrome, 0 if not
  */
 
 int is_palindrome(char *s)
 {
-	int get_leng(char *s);
-	int my_find(char *s, int 1);
+	int get_length(char *s);
+	int my_pal(char *s, int l);
 	int length;
 
-	length = get_leng(s) - 1;
+	length = get_length(s) - 1;
 
-	return (my_find(s, --length));
+	return (my_pal(s, --length));
 }
 
 /**
- * get_leng - gets the length of a string
+ * get_length - gets length of string
  * @s: string
- * Return: returns the length of the string
+ * Return: return length of string
  */
 
-
-int get_leng(char *s)
+int get_length(char *s)
 {
 	if (*s == '\0')
 		return (1);
 	else
-		return (1 + get_leng(++s));
+		return (1 + get_length(++s));
 }
 
 /**
- * my_find - recursive for finding palindrome
+ * my_pal - recursive check of palindrome
  * @s: string
- * @l: length of the string
- * Return: returns 1 if true
+ * @l: length of string
+ * Return: 1 if palindrome, 0 if not
  */
 
-int my_find(char *s, int l)
+int my_pal(char *s, int l)
 {
 	if (*s == *(s + l))
 	{
 		if (l <= 0)
 			return (1);
 		else
-			return (my_find(++s, l - 2));
+			return (my_pal(++s, l - 2));
 	}
 	else
 		return (0);
