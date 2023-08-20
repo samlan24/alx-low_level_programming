2de1bc8957be4e87950440ea1b3d6bc1a9b22fe9
samlan24 <muchemionline@gmail.com>
2023-08-20 09:34:29 -0700

    3-add_dnodeint_end.c

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -19,20 +19,20 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	new->n = n;
 	new->next = NULL;
 
-	if (*head == NULL)
-	{
-		new->prev = NULL;
-		*head = new;
-		return (new);
-	}
-
 	current = *head;
 
-	while (current->next != NULL)
-		current = current->mext;
+	if (current != NULL)
+	{
+		while (current->next != NULL)
+			current = current->next;
+		current->next = new;
+	}
+	else
+	{
+		*head = new;
+	}
 
 	new->prev = current;
-	current->next = new;
 
 	return (new);
 }
