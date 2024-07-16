f30c7bcdd243de4aeb47d684724ec2161c414c7b
samlan24 <muchemionline@gmail.com>
2024-07-15 23:46:38 -0700

    updates

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -9,13 +9,13 @@
 
 int linear_search(int *array, size_t size, int value)
 {
-    if (array == NULL || value < 0)
-        return (-1);
+	if (array == NULL || value < 0)
+		return (-1);
 
-    for (size_t i = 0; i <  size; i++)
-    {
-        if (array[i] == value)
-            return (i);
-    }
-    return (-1);
+	for (size_t i = 0; i < size; i++)
+	{
+		if (array[i] == value)
+			return (i);
+	}
+	return (-1);
 }
